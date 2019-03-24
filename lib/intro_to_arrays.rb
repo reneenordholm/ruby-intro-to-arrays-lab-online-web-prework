024ea960d68f31481b180d3dc08f5d5b54d2e0b2
Renee Nordholm <reneenordholm@gmail.com>
2019-03-24 13:46:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,3 +15,8 @@ def third_element(taylor_swift)
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[2]
 end
+
+def last_element(taylor_swift)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift[-1]
+end
